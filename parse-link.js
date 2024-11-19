4cb553bbba6476c83a9c40c7205a3e79f6ca180b
dmc <david.mark.clements@gmail.com>
2024-11-19 20:38:53 +0100

    errors, state, parse-link

---
+++ parse-link.js
@@ -0,0 +1,6 @@
+'use strict'
+const constants = require('./constants')
+const { ERR_INVALID_LINK } = require('./errors')
+const pearLink = require('pear-link')
+const parse = pearLink(constants.ALIASES, ERR_INVALID_LINK)
+module.exports = parse
