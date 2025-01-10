5adf0d2a537b9fb4797f193ebb2d91c2032d2385
Marco <maidh91@gmail.com>
2025-01-11 09:25:31 +1100

    init-test

---
+++ test/state.test.js
@@ -0,0 +1,34 @@
+'use strict'
+const test = require('brittle')
+const { command, flag } = require('paparam')
+
+// mock global.Pear
+global.Pear = {
+  constructor: {
+    CHECKOUT: {
+      length: null
+    }
+  }
+}
+
+const rundef = require('../cmd/run')
+const State = require('../state')
+
+test('new State with runtime info', async (t) => {
+  t.plan(2)
+
+  const run = command('run', ...rundef, runMain)
+  run.parse([
+    '--runtime-info', '{ "type": "bridge", "data": "http://localhost:1234" }',
+    'pear://link'
+  ])
+
+  function runMain (cmd) {
+    const state = new State({
+      flags: cmd.flags,
+      args: cmd.args,
+    })
+    t.is(state.runtimeInfo.type, 'bridge')
+    t.is(state.runtimeInfo.data, 'http://localhost:1234')
+  }
+})
