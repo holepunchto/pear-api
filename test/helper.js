f61bbc7fcdab3a6c61163cd31993fe1bd52424c9
Marco <maidh91@gmail.com>
2025-02-11 05:11:20 +1100

    lint

--- test/helper.js
+++ test/helper.js
@@ -1,7 +1,7 @@
 const dirname = __dirname
 
 class Helper {
-  static rig() {
+  static rig () {
     class TestAPI {
       static RTI = { checkout: { key: dirname, length: null, fork: null } }
       static get CONSTANTS () { return require('../constants') }
