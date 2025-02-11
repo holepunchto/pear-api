e8bc5208de60a3ae0c3c81e44695082811ae0ce1
Marco <maidh91@gmail.com>
2025-02-12 02:59:39 +1100

    clean

--- test/helper.js
+++ test/helper.js
@@ -9,26 +9,6 @@ class Helper {
     }
     global.Pear = new TestAPI()
   }
-
-  static rigBareWorker () {
-    const Worker = require('../worker')
-    Worker.RUNTIME = Bare.argv[0]
-    Worker.RUNTIME_PARSER = command('bare', ...rundef)
-    Worker.RUNTIME_ARGS = []
-    const worker = new Worker({ ref: () => undefined, unref: () => undefined })
-    return worker
-  }
-
-  static rigBareAPI () {
-    const ipc = {
-      ref: () => undefined,
-      unref: () => undefined
-    }
-    const state = {}
-    const worker = Helper.rigBareWorker()
-    const API = require('..')
-    global.Pear = new API(ipc, state, { worker })
-  }
 }
 
 module.exports = Helper
