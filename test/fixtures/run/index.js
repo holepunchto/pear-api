2206fea1d4f0cef24e9ff927c2de730f83ae580a
Marco <maidh91@gmail.com>
2025-02-12 20:01:11 +1100

    500ms

--- test/fixtures/run/index.js
+++ test/fixtures/run/index.js
@@ -18,7 +18,7 @@ let interval = null
 pipe.on('data', (data) => {
   const str = data.toString()
   if (str === 'ping') {
-    interval = setInterval(() => pipe.write((i++).toString()), 2000)
+    interval = setInterval(() => pipe.write((i++).toString()), 500)
   }
   if (str === 'exit') {
     clearInterval(interval)
--- test/fixtures/worker/index.js
+++ test/fixtures/worker/index.js
@@ -21,7 +21,7 @@ let interval = null
 pipe.on('data', (data) => {
   const str = data.toString()
   if (str === 'ping') {
-    interval = setInterval(() => pipe.write((i++).toString()), 2000)
+    interval = setInterval(() => pipe.write((i++).toString()), 500)
   }
   if (str === 'exit') {
     clearInterval(interval)
