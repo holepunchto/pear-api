84d1cf2dbda724e9f224e1d2bca20a95369a6b68
Marco <maidh91@gmail.com>
2025-02-19 21:26:58 +1100

    add messages multiple test

--- test/messages.test.js
+++ test/messages.test.js
@@ -7,8 +7,35 @@ const Helper = require('./helper')
 
 const noop = () => undefined
 
-test('messages pattern', async function (t) {
+test('messages single', async function (t) {
   t.plan(2)
+
+  const bus = new Iambus()
+  t.teardown(() => bus.destroy())
+
+  const ipc = {
+    ref: noop,
+    unref: noop,
+    messages: (pattern) => bus.sub(pattern)
+  }
+  const teardown = Helper.rig({ ipc })
+  t.teardown(teardown)
+
+  const promise = new Promise((resolve) => {
+    const sub = Pear.messages({ hello: 'world' }, (msg) => {
+      sub.end()
+      resolve(msg)
+    })
+  })
+  bus.pub({ hello: 'world', time: Date.now() })
+
+  const msg = await promise
+  t.is(msg.hello, 'world')
+  t.ok(typeof msg.time === 'number' && msg.time <= Date.now())
+})
+
+test('messages multiple', async function (t) {
+  t.plan(3)
 
   const bus = new Iambus()
   t.teardown(() => bus.destroy())
@@ -22,13 +49,20 @@ test('messages pattern', async function (t) {
   const teardown = Helper.rig({ ipc })
   t.teardown(teardown)
 
-  const msg = await new Promise((resolve) => {
+  const promise = new Promise((resolve) => {
+    const messages = []
     const sub = Pear.messages({ hello: 'world' }, (msg) => {
-      clearInterval(interval)
-      sub.end()
-      resolve(msg)
+      messages.push(msg)
+      if (messages.length === 4) {
+        clearInterval(interval)
+        sub.end()
+        resolve(messages)
+      }
     })
   })
-  t.is(msg.hello, 'world')
-  t.ok(typeof msg.time === 'number' && msg.time <= Date.now())
+
+  const messages = await promise
+  t.is(messages.length, 4)
+  t.ok(messages.every(msg => msg.hello === 'world'))
+  t.ok(messages.every(msg => typeof msg.time === 'number' && msg.time <= Date.now()))
 })
