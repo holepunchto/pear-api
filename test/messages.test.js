9a846e38596317d147d5708f39c25b1fc9781230
Marco <maidh91@gmail.com>
2025-02-19 21:29:03 +1100

    messages legacy

--- test/messages.test.js
+++ test/messages.test.js
@@ -66,3 +66,30 @@ test('messages multiple', async function (t) {
   t.ok(messages.every(msg => msg.hello === 'world'))
   t.ok(messages.every(msg => typeof msg.time === 'number' && msg.time <= Date.now()))
 })
+
+test('messages legacy', async function (t) {
+  t.plan(2)
+
+  const bus = new Iambus()
+  t.teardown(() => bus.destroy())
+
+  const ipc = {
+    ref: noop,
+    unref: noop,
+    messages: (pattern) => bus.sub(pattern)
+  }
+  const teardown = Helper.rig({ ipc })
+  t.teardown(teardown)
+
+  const promise = new Promise((resolve) => {
+    const sub = Pear.messages((msg) => {
+      sub.end()
+      resolve(msg)
+    })
+  })
+  bus.pub({ hello: 'world', time: Date.now() })
+
+  const msg = await promise
+  t.is(msg.hello, 'world')
+  t.ok(typeof msg.time === 'number' && msg.time <= Date.now())
+})
