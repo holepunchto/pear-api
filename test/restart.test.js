43c4270dd610db712193d5c1011a069be8fe2958
Marco <maidh91@gmail.com>
2025-02-27 21:29:08 +1100

    restart ok

--- test/restart.test.js
+++ test/restart.test.js
@@ -12,3 +12,22 @@ test('restart terminal app throw error', async function (t) {
 
   t.exception(() => Pear.restart(), 'Pear.restart threw an error for terminal app')
 })
+
+test('restart ok', async function (t) {
+  t.plan(2)
+
+  await Helper.startIpcServer({
+    handlers: {
+      restart: (opts) => ({ ...opts, time: Date.now() })
+    },
+    teardown: t.teardown
+  })
+  const ipc = await Helper.startIpcClient()
+
+  const teardown = Helper.rig({ ipc })
+  t.teardown(teardown)
+
+  const res = await Pear.restart({ hello: 'world' })
+  t.ok(res.hello === 'world', 'restart returned')
+  t.ok(typeof res.time === 'number' && res.time <= Date.now(), 'restart has time')
+})
