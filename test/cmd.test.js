ff86baeac360244a0b3c890b6fbeccfe6e81a4fb
Marco <maidh91@gmail.com>
2025-03-11 21:02:44 +1100

    cmd test

--- test/cmd.test.js
+++ test/cmd.test.js
@@ -3,5 +3,12 @@
 const { test } = require('brittle')
 
 test('cmd', async function (t) {
-  // TODO: write tests
+  t.plan(3)
+
+  const cmd = require('../cmd')
+  const res = cmd(['--log', 'run', 'pear://runtime'])
+
+  t.ok(res.flags.log === true)
+  t.ok(res.args.cmd === 'run')
+  t.ok(res.rest[0] === 'pear://runtime')
 })
