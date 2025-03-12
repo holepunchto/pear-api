e779c4fa0da5c9efac5e8f6ba5a77f8b38aa8415
Marco <maidh91@gmail.com>
2025-03-12 22:13:48 +1100

    terminal indicator

--- test/terminal.test.js
+++ test/terminal.test.js
@@ -2,6 +2,31 @@
 
 const { test } = require('brittle')
 
-test('terminal', async function (t) {
-  // TODO: write tests
+const dirname = __dirname
+global.Pear = null
+
+const rig = () => {
+  if (global.Pear !== null) throw Error(`Prior Pear global not cleaned up: ${global.Pear}`)
+
+  class RigAPI {
+    static RTI = { checkout: { key: dirname, length: null, fork: null } }
+  }
+  global.Pear = new RigAPI()
+
+  return {
+    teardown: () => { global.Pear = null }
+  }
+}
+
+test('terminal indicator', async function (t) {
+  t.plan(4)
+
+  const { teardown } = rig()
+  t.teardown(teardown)
+
+  const { indicator, ansi } = require('../terminal')
+  t.ok(indicator() === '')
+  t.ok(indicator(null) === ansi.gray('- '))
+  t.ok(indicator(true) === ansi.tick + ' ')
+  t.ok(indicator(false) === ansi.cross + ' ')
 })
