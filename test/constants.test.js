29edb987121333aef2d148788b510ffe37f7b0ba
Marco <maidh91@gmail.com>
2025-03-14 18:43:43 +1100

    constant tests

--- test/constants.test.js
+++ test/constants.test.js
@@ -5,21 +5,24 @@ const { test } = require('brittle')
 const dirname = __dirname
 global.Pear = null
 
-const rig = () => {
-  if (global.Pear !== null) throw Error(`Prior Pear global not cleaned up: ${global.Pear}`)
-
-  class RigAPI {
-    static RTI = { checkout: { key: dirname, length: null, fork: null } }
-  }
-  global.Pear = new RigAPI()
-
-  return {
-    teardown: () => { global.Pear = null }
-  }
-}
-
-test('constants CHECKOUT', async function (t) {
+test('constants with CHECKOUT', async function (t) {
   t.plan(3)
+
+  const rig = () => {
+    if (global.Pear !== null) throw Error(`Prior Pear global not cleaned up: ${global.Pear}`)
+
+    class RigAPI {
+      static RTI = { checkout: { key: dirname, length: null, fork: null } }
+    }
+    global.Pear = new RigAPI()
+
+    return {
+      teardown: () => {
+        delete require.cache[`file://${require.resolve('../constants')}`]
+        global.Pear = null
+      }
+    }
+  }
 
   const { teardown } = rig()
   t.teardown(teardown)
@@ -28,4 +31,185 @@ test('constants CHECKOUT', async function (t) {
   t.ok(constants.CHECKOUT.key === dirname)
   t.ok(constants.CHECKOUT.length === null)
   t.ok(constants.CHECKOUT.fork === null)
+})
+
+test('constants with default MOUNT', async function (t) {
+  t.plan(1)
+
+  const rig = () => {
+    if (global.Pear !== null) throw Error(`Prior Pear global not cleaned up: ${global.Pear}`)
+
+    class RigAPI {
+      static RTI = { checkout: { key: dirname, length: null, fork: null } }
+    }
+    global.Pear = new RigAPI()
+
+    return {
+      teardown: () => {
+        delete require.cache[`file://${require.resolve('../constants')}`]
+        global.Pear = null
+      }
+    }
+  }
+
+  const { teardown } = rig()
+  t.teardown(teardown)
+
+  const constants = require('../constants')
+  t.ok(constants.MOUNT === `file://${dirname}`)
+})
+
+test('constants with MOUNT starting with c:', async function (t) {
+  t.plan(1)
+
+  const rig = () => {
+    if (global.Pear !== null) throw Error(`Prior Pear global not cleaned up: ${global.Pear}`)
+
+    class RigAPI {
+      static RTI = { checkout: { key: dirname, length: null, fork: null }, mount: 'c:/custom/mount' }
+    }
+    global.Pear = new RigAPI()
+
+    return {
+      teardown: () => {
+        delete require.cache[`file://${require.resolve('../constants')}`]
+        global.Pear = null
+      }
+    }
+  }
+
+  const { teardown } = rig()
+  t.teardown(teardown)
+
+  const constants = require('../constants')
+  t.ok(constants.MOUNT === 'file:///c:/custom/mount')
+})
+
+test('constants with MOUNT starting with file:', async function (t) {
+  t.plan(1)
+
+  const rig = () => {
+    if (global.Pear !== null) throw Error(`Prior Pear global not cleaned up: ${global.Pear}`)
+
+    class RigAPI {
+      static RTI = { checkout: { key: dirname, length: null, fork: null }, mount: 'file:///custom/mount' }
+    }
+    global.Pear = new RigAPI()
+
+    return {
+      teardown: () => {
+        delete require.cache[`file://${require.resolve('../constants')}`]
+        global.Pear = null
+      }
+    }
+  }
+
+  const { teardown } = rig()
+  t.teardown(teardown)
+
+  const constants = require('../constants')
+  t.ok(constants.MOUNT === 'file:///custom/mount')
+})
+
+test('constants with MOUNT starting with ./', async function (t) {
+  t.plan(1)
+
+  const rig = () => {
+    if (global.Pear !== null) throw Error(`Prior Pear global not cleaned up: ${global.Pear}`)
+
+    class RigAPI {
+      static RTI = { checkout: { key: dirname, length: null, fork: null }, mount: './custom/mount' }
+    }
+    global.Pear = new RigAPI()
+
+    return {
+      teardown: () => {
+        delete require.cache[`file://${require.resolve('../constants')}`]
+        global.Pear = null
+      }
+    }
+  }
+
+  const { teardown } = rig()
+  t.teardown(teardown)
+
+  const constants = require('../constants')
+  t.ok(constants.MOUNT === 'file:///custom/mount')
+})
+
+test('constants with MOUNT starting with ../', async function (t) {
+  t.plan(1)
+
+  const rig = () => {
+    if (global.Pear !== null) throw Error(`Prior Pear global not cleaned up: ${global.Pear}`)
+
+    class RigAPI {
+      static RTI = { checkout: { key: dirname, length: null, fork: null }, mount: '../custom/mount' }
+    }
+    global.Pear = new RigAPI()
+
+    return {
+      teardown: () => {
+        delete require.cache[`file://${require.resolve('../constants')}`]
+        global.Pear = null
+      }
+    }
+  }
+
+  const { teardown } = rig()
+  t.teardown(teardown)
+
+  const constants = require('../constants')
+  t.ok(constants.MOUNT === 'file:///custom/mount')
+})
+
+test('constants with MOUNT starting with /', async function (t) {
+  t.plan(1)
+
+  const rig = () => {
+    if (global.Pear !== null) throw Error(`Prior Pear global not cleaned up: ${global.Pear}`)
+
+    class RigAPI {
+      static RTI = { checkout: { key: dirname, length: null, fork: null }, mount: '/custom/mount' }
+    }
+    global.Pear = new RigAPI()
+
+    return {
+      teardown: () => {
+        delete require.cache[`file://${require.resolve('../constants')}`]
+        global.Pear = null
+      }
+    }
+  }
+
+  const { teardown } = rig()
+  t.teardown(teardown)
+
+  const constants = require('../constants')
+  t.ok(constants.MOUNT === 'file:///custom/mount')
+})
+
+test('constants with MOUNT starting with pear://', async function (t) {
+  t.plan(1)
+
+  const rig = () => {
+    if (global.Pear !== null) throw Error(`Prior Pear global not cleaned up: ${global.Pear}`)
+
+    class RigAPI {
+      static RTI = { checkout: { key: dirname, length: null, fork: null }, mount: 'pear://custom/mount' }
+    }
+    global.Pear = new RigAPI()
+
+    return {
+      teardown: () => {
+        delete require.cache[`file://${require.resolve('../constants')}`]
+        global.Pear = null
+      }
+    }
+  }
+
+  const { teardown } = rig()
+  t.teardown(teardown)
+
+  t.exception(() => require('../constants'))
 })
