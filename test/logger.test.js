f0e76ba0053e9ea05f348e22feb203af50d2bedf
Marco <maidh91@gmail.com>
2025-03-14 21:07:12 +1100

    add logger tests

--- test/logger.test.js
+++ test/logger.test.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const { test } = require('brittle')
+const Logger = require('../logger')
 
 const consoleLogOrigin = console.log
 
@@ -14,4 +15,38 @@ test('logger', async function (t) {
   const logger = new Logger({ labels: ['label-test'], level: Logger.INF })
   t.exception(() => logger.info('label-test', 'hello'), 'console.log called hello')
   logger.error('label-random', 'world')
+})
+
+test('logger error method', async function (t) {
+  t.plan(1)
+
+  console.error = (msg) => { throw new Error(`console.error called ${msg}`) }
+  t.teardown(() => { console.error = consoleLogOrigin })
+
+  const logger = new Logger({ labels: ['label-test'], level: Logger.ERR })
+  t.exception(() => logger.error('label-test', 'error message'), 'console.error called error message')
+  logger.info('label-random', 'info message')
+})
+
+test('logger trace method', async function (t) {
+  t.plan(1)
+
+  console.error = (msg) => { throw new Error(`console.error called ${msg}`) }
+  t.teardown(() => { console.error = consoleLogOrigin })
+
+  const logger = new Logger({ labels: ['label-test'], level: Logger.TRC })
+  t.exception(() => logger.trace('label-test', 'trace message'), 'console.error called trace message')
+  logger.info('label-random', 'info message')
+})
+
+test('logger with multiple labels', async function (t) {
+  t.plan(2)
+
+  console.log = (msg) => { throw new Error(`console.log called ${msg}`) }
+  t.teardown(() => { console.log = consoleLogOrigin })
+
+  const logger = new Logger({ labels: ['label-test', 'label-extra'], level: Logger.INF })
+  t.exception(() => logger.info('label-test', 'hello'), 'console.log called hello')
+  t.exception(() => logger.info('label-extra', 'world'), 'console.log called world')
+  logger.error('label-random', 'error message')
 })
