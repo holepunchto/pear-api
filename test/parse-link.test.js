4b1e6fe748f2a9d0f4b011881e326ed4f717d32f
Marco <maidh91@gmail.com>
2025-03-19 21:55:33 +1100

    add more parse link test

--- test/parse-link.test.js
+++ test/parse-link.test.js
@@ -36,7 +36,7 @@ test('parse-link file:///some/path/to/a/file.js', async function (t) {
   t.ok(res.drive.hash === null)
 })
 
-test('parse-link pear://<key>', async function (t) {
+test('parse-link pear://key', async function (t) {
   t.plan(8)
 
   const { teardown } = rig()
@@ -56,8 +56,8 @@ test('parse-link pear://<key>', async function (t) {
   t.ok(res.drive.alias === null)
 })
 
-test('parse-link alias pear://keet', async function (t) {
-  t.plan(8)
+test('parse-link alias pear://alias', async function (t) {
+  t.plan(24)
 
   const { teardown } = rig()
   t.teardown(teardown)
@@ -65,20 +65,42 @@ test('parse-link alias pear://keet', async function (t) {
   const parseLink = require('../parse-link')
   const constants = require('../constants')
 
-  const key = 'keet'
-  const res = parseLink(`pear://${key}`)
+  const aliases = ['keet', 'runtime', 'doctor']
+  for (const alias of aliases) {
+    const res = parseLink(`pear://${alias}`)
+    t.ok(res.protocol === 'pear:')
+    t.ok(res.pathname === '')
+    t.ok(res.hash === '')
+    t.ok(res.drive.key === constants.ALIASES[alias])
+    t.ok(res.drive.length === 0)
+    t.ok(res.drive.fork === null)
+    t.ok(res.drive.hash === null)
+    t.ok(res.drive.alias === alias)
+  }
+})
+
+test('parse-link pear://fork.length.key', async function (t) {
+  t.plan(8)
+
+  const { teardown } = rig()
+  t.teardown(teardown)
+
+  const parseLink = require('../parse-link')
+
+  const key = 'd47c1dfecec0f74a067985d2f8d7d9ad15f9ae5ff648f7bc6ca28e41d70ed221'
+  const res = parseLink(`pear://123.456.${key}`)
   t.ok(res.protocol === 'pear:')
   t.ok(res.pathname === '')
   t.ok(res.hash === '')
-  t.ok(res.drive.key === constants.ALIASES[key])
-  t.ok(res.drive.length === 0)
-  t.ok(res.drive.fork === null)
+  t.ok(res.drive.key.toString('hex') === key)
+  t.ok(res.drive.length === 456)
+  t.ok(res.drive.fork === 123)
   t.ok(res.drive.hash === null)
-  t.ok(res.drive.alias === key)
+  t.ok(res.drive.alias === null)
 })
 
-test('parse-link alias pear://runtime', async function (t) {
-  t.plan(8)
+test('parse-link alias pear://fork.length.alias', async function (t) {
+  t.plan(24)
 
   const { teardown } = rig()
   t.teardown(teardown)
@@ -86,46 +108,55 @@ test('parse-link alias pear://runtime', async function (t) {
   const parseLink = require('../parse-link')
   const constants = require('../constants')
 
-  const key = 'runtime'
-  const res = parseLink(`pear://${key}`)
-  t.ok(res.protocol === 'pear:')
-  t.ok(res.pathname === '')
-  t.ok(res.hash === '')
-  t.ok(res.drive.key === constants.ALIASES[key])
-  t.ok(res.drive.length === 0)
-  t.ok(res.drive.fork === null)
-  t.ok(res.drive.hash === null)
-  t.ok(res.drive.alias === key)
+  const aliases = ['keet', 'runtime', 'doctor']
+  for (const alias of aliases) {
+    const res = parseLink(`pear://123.456.${alias}`)
+    t.ok(res.protocol === 'pear:')
+    t.ok(res.pathname === '')
+    t.ok(res.hash === '')
+    t.ok(res.drive.key === constants.ALIASES[alias])
+    t.ok(res.drive.length === 456)
+    t.ok(res.drive.fork === 123)
+    t.ok(res.drive.hash === null)
+    t.ok(res.drive.alias === alias)
+  }
 })
 
-test('parse-link alias pear://doctor', async function (t) {
+test('parse-link pear://fork.length.key.dhash', async function (t) {
   t.plan(8)
 
   const { teardown } = rig()
   t.teardown(teardown)
 
   const parseLink = require('../parse-link')
-  const constants = require('../constants')
 
-  const key = 'doctor'
-  const res = parseLink(`pear://${key}`)
+  const key = 'd47c1dfecec0f74a067985d2f8d7d9ad15f9ae5ff648f7bc6ca28e41d70ed221'
+  const dhash = '38d8296e972167f4ad37803999fbcac17025271162f44dcdce1188d4bc5bac1d'
+  const res = parseLink(`pear://123.456.${key}.${dhash}`)
   t.ok(res.protocol === 'pear:')
   t.ok(res.pathname === '')
   t.ok(res.hash === '')
-  t.ok(res.drive.key === constants.ALIASES[key])
-  t.ok(res.drive.length === 0)
-  t.ok(res.drive.fork === null)
-  t.ok(res.drive.hash === null)
-  t.ok(res.drive.alias === key)
+  t.ok(res.drive.key.toString('hex') === key)
+  t.ok(res.drive.length === 456)
+  t.ok(res.drive.fork === 123)
+  t.ok(res.drive.hash.toString('hex') === dhash)
+  t.ok(res.drive.alias === null)
 })
 
-test('parse-link error', async function (t) {
-  t.plan(1)
+test('parse-link incorrect hostname', async function (t) {
+  t.plan(8)
 
   const { teardown } = rig()
   t.teardown(teardown)
 
   const parseLink = require('../parse-link')
 
-  t.exception(() => parseLink('pear://unknown'))
+  t.exception(() => parseLink('pear://invalid-key'))
+  t.exception(() => parseLink('pear://123.456'))
+  t.exception(() => parseLink('pear://123.nan.d47c1dfecec0f74a067985d2f8d7d9ad15f9ae5ff648f7bc6ca28e41d70ed221'))
+  t.exception(() => parseLink('pear://nan.123.keet'))
+  t.exception(() => parseLink('pear://123.nan.d47c1dfecec0f74a067985d2f8d7d9ad15f9ae5ff648f7bc6ca28e41d70ed221.38d8296e972167f4ad37803999fbcac17025271162f44dcdce1188d4bc5bac1d'))
+  t.exception(() => parseLink('pear://nan.123.keet.38d8296e972167f4ad37803999fbcac17025271162f44dcdce1188d4bc5bac1d'))
+  t.exception(() => parseLink('pear://a.b.c.d.e'))
+  t.exception(() => parseLink('unsupport://abc'))
 })
