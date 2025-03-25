7cfa2c6857a8ac5cf73b2916e507abac0e724647
Marco <maidh91@gmail.com>
2025-03-26 05:15:27 +1100

    add captureStackTrace

--- test/errors.test.js
+++ test/errors.test.js
@@ -39,3 +39,16 @@ test('known function with no matching prefix', async function (t) {
   t.ok(Array.isArray(knownErrors))
   t.is(knownErrors.length, 0)
 })
+
+test('captureStackTrace filters out PearError only', async function (t) {
+  t.plan(2)
+
+  const pearErr = PearError.ERR_INVALID_INPUT('invalid input')
+  t.is(pearErr.stack.includes('ERR_INVALID_INPUT'), false)
+
+  function ERR_CUSTOM (msg) {
+    return new Error(msg, 'ERR_CUSTOM', ERR_CUSTOM)
+  }
+  const customErr = ERR_CUSTOM('custom error')
+  t.is(customErr.stack.includes('ERR_CUSTOM'), true)
+})
