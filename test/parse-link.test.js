2ac62215d734fa1d5db5584c7fc0db84b59bc84a
Marco <maidh91@gmail.com>
2025-03-26 04:36:07 +1100

    more parse link tests

--- test/parse-link.test.js
+++ test/parse-link.test.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const { test } = require('brittle')
+const path = require('path')
 
 const dirname = __dirname
 global.Pear = null
@@ -17,6 +18,24 @@ const rig = () => {
     teardown: () => { global.Pear = null }
   }
 }
+
+test('parse-link ./some/path/to/a/file.js', async function (t) {
+  t.plan(7)
+
+  const { teardown } = rig()
+  t.teardown(teardown)
+
+  const parseLink = require('../parse-link')
+
+  const res = parseLink('./some/path/to/a/file.js')
+  t.is(res.protocol, 'file:')
+  t.is(res.pathname, path.resolve('./some/path/to/a/file.js'))
+  t.is(res.hash, '')
+  t.is(res.drive.key, null)
+  t.is(res.drive.length, null)
+  t.is(res.drive.fork, null)
+  t.is(res.drive.hash, null)
+})
 
 test('parse-link file:///some/path/to/a/file.js', async function (t) {
   t.plan(7)
@@ -143,20 +162,23 @@ test('parse-link pear://fork.length.key.dhash', async function (t) {
   t.is(res.drive.alias, null)
 })
 
-test('parse-link incorrect hostname', async function (t) {
-  t.plan(8)
+test.solo('parse-link invalid link', async function (t) {
+  // t.plan(10)
 
   const { teardown } = rig()
   t.teardown(teardown)
 
   const parseLink = require('../parse-link')
+  const { ERR_INVALID_LINK } = require('../errors')
 
+  t.exception(() => parseLink(), ERR_INVALID_LINK())
+  t.exception(() => parseLink(''), ERR_INVALID_LINK())
   t.exception(() => parseLink('pear://invalid-key'))
-  t.exception(() => parseLink('pear://123.456'))
-  t.exception(() => parseLink('pear://123.nan.d47c1dfecec0f74a067985d2f8d7d9ad15f9ae5ff648f7bc6ca28e41d70ed221'))
-  t.exception(() => parseLink('pear://nan.123.keet'))
-  t.exception(() => parseLink('pear://123.nan.d47c1dfecec0f74a067985d2f8d7d9ad15f9ae5ff648f7bc6ca28e41d70ed221.38d8296e972167f4ad37803999fbcac17025271162f44dcdce1188d4bc5bac1d'))
-  t.exception(() => parseLink('pear://nan.123.keet.38d8296e972167f4ad37803999fbcac17025271162f44dcdce1188d4bc5bac1d'))
-  t.exception(() => parseLink('pear://a.b.c.d.e'))
-  t.exception(() => parseLink('unsupport://abc'))
+  t.exception(() => parseLink('pear://123.456'), ERR_INVALID_LINK())
+  t.exception(() => parseLink('pear://123.nan.d47c1dfecec0f74a067985d2f8d7d9ad15f9ae5ff648f7bc6ca28e41d70ed221'), ERR_INVALID_LINK())
+  t.exception(() => parseLink('pear://nan.123.keet'), ERR_INVALID_LINK())
+  t.exception(() => parseLink('pear://123.nan.d47c1dfecec0f74a067985d2f8d7d9ad15f9ae5ff648f7bc6ca28e41d70ed221.38d8296e972167f4ad37803999fbcac17025271162f44dcdce1188d4bc5bac1d'), ERR_INVALID_LINK())
+  t.exception(() => parseLink('pear://nan.123.keet.38d8296e972167f4ad37803999fbcac17025271162f44dcdce1188d4bc5bac1d'), ERR_INVALID_LINK())
+  t.exception(() => parseLink('pear://a.b.c.d.e'), ERR_INVALID_LINK())
+  t.exception(() => parseLink('unsupport://abc'), ERR_INVALID_LINK())
 })
