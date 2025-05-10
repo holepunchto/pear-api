d9b9fbd20f8f77a74e9390b56add388017534677
Jan Keith Darunday <jkcdarunday@gmail.com>
2025-05-10 19:14:02 +0800

    Use Helper.override in the tryboot fixture

--- test/fixtures/tryboot/index.js
+++ test/fixtures/tryboot/index.js
@@ -7,13 +7,12 @@ let resolve = () => {}
 const spawnCalled = new Promise((_resolve) => {
   resolve = _resolve
 })
-const childProcess = require('child_process')
-const originalSpawn = childProcess.spawn
-childProcess.spawn = (cmd, args, options) => {
-  resolve({ cmd, args, options })
-  return { unref: () => {} }
-}
-Pear.teardown(() => { childProcess.spawn = originalSpawn })
+Pear.teardown(Helper.override('child_process', {
+  spawn: (cmd, args, options) => {
+    resolve({ cmd, args, options })
+    return { unref: () => {} }
+  }
+}))
 
 const tryboot = require('../../../tryboot')
 tryboot()
