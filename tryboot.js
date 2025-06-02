7a54e8659857f122621d28ee76e7b6dc08f5c514
Jan Keith Darunday <jkcdarunday@gmail.com>
2025-06-02 22:54:52 +0800

    Remove unused flags variable

--- tryboot.js
+++ tryboot.js
@@ -1,11 +1,9 @@
 'use strict'
 const daemon = require('bare-daemon')
 const { RUNTIME, PLATFORM_DIR } = require('./constants')
-const pear = require('./cmd')
 
 module.exports = function tryboot () {
   const { argv } = global.Bare || global.process
-  const { flags = {} } = pear(argv.slice(1)) ?? {}
   const args = ['--sidecar']
   const dhtBootstrap = argv.includes('--dht-bootstrap') ? argv[argv.indexOf('--dht-bootstrap') + 1] : null
   if (dhtBootstrap) {
