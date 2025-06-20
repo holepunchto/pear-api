ca9fe80b5af726dcf6143cb5a266d874fa49c9c9
Geordan Gesink <georgegeordan@gmail.com>
2025-06-20 20:53:28 +0200

    test err with exception

--- test/teardown.test.js
+++ test/teardown.test.js
@@ -1,13 +1,14 @@
 'use strict'
 
 const { test } = require('brittle')
+const { isWindows, isBare } = require('which-runtime')
 const path = require('path')
 
 const Helper = require('./helper')
 
 const dirname = __dirname
 
-test('teardown default', async function (t) {
+test('teardown default', { skip: !isBare || isWindows }, async function (t) {
   t.plan(1)
 
   const dir = path.join(dirname, 'fixtures', 'teardown-default')
@@ -21,7 +22,7 @@ test('teardown default', async function (t) {
   t.is(td, 'teardown', 'teardown executed')
 })
 
-test('teardown with position', async function (t) {
+test('teardown with position', { skip: !isBare || isWindows }, async function (t) {
   t.plan(1)
 
   const dir = path.join(dirname, 'fixtures', 'teardown-with-position')
@@ -33,4 +34,20 @@ test('teardown with position', async function (t) {
 
   const td = await Helper.untilResult(pipe, { runFn: () => pipe.end() })
   t.is(td, 'teardown', 'teardown executed')
+})
+
+test('teardown with type err in first arg', { skip: !isBare || isWindows }, async function (t) {
+  t.plan(1)
+  const fnErr = `First argument of Pear.teardown must be a function, recieved type 'string'`
+  const teardown = Helper.rig({ clearRequireCache: '../teardown' })
+  t.teardown(teardown)
+  t.exception(() => Pear.teardown('notAFunction'), fnErr)
+})
+
+test('teardown with type err in first arg', { skip: !isBare || isWindows }, async function (t) {
+  t.plan(1)
+  const positionErr = `Second argument of Pear.teardown must be an integer or ±Infinity, recieved type 'string'`
+  const teardown = Helper.rig({ clearRequireCache: '../teardown' })
+  t.teardown(teardown)
+  t.exception(() => Pear.teardown(()=>{}, '5'), positionErr)
 })
