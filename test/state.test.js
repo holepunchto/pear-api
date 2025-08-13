256c327d450040ca537761e3b8cd40cd7a728822
dmc <david.mark.clements@gmail.com>
2025-08-13 22:55:52 +0200

    pear-crasher

--- test/state.test.js
+++ test/state.test.js
@@ -373,7 +373,7 @@ test('state.applink', async function (t) {
 
   const helloWorld = path.join(dirname, 'fixtures', 'hello-world')
 
-  const State = require('..')
+  const State = require('pear-state')
   const cwd = global.process?.cwd ?? os.cwd
   t.is(new State({ dir: helloWorld, link: helloWorld, flags: {} }).applink, pathToFileURL(helloWorld).href)
   t.is(new State({ dir: helloWorld, link: helloWorld + '/some/route', flags: {} }).applink, pathToFileURL(helloWorld).href)
