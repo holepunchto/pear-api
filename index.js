19463a14b8d5792b72514355a906e13d553c1e1d
Mikkel Malmberg <mikkel@brnbw.com>
2025-12-05 10:56:47 +0100

    Add linter and update CI

--- index.js
+++ index.js
@@ -79,24 +79,27 @@ class API {
   }
 
   get worker() {
-    if (!this.constructor.COMPAT)
+    if (!this.constructor.COMPAT) {
       console.error(
         '[ DEPRECATED ] Pear.worker is deprecated and will be removed. Use pear-run & pear-pipe'
       )
+    }
     return new (class DeprecatedWorker {
       pipe() {
-        if (!this.constructor.COMPAT)
+        if (!this.constructor.COMPAT) {
           console.error(
             '[ DEPRECATED ] Pear.worker.pipe() is deprecated. Use pear-pipe'
           )
+        }
         return pipe()
       }
 
       run(link, args = []) {
-        if (!this.constructor.COMPAT)
+        if (!this.constructor.COMPAT) {
           console.error(
             '[ DEPRECATED ] Pear.worker.run() is deprecated. Use pear-run'
           )
+        }
         return run(link, args)
       }
     })()
@@ -108,25 +111,29 @@ class API {
   }
 
   restart = async (opts = {}) => {
-    if (!this.constructor.COMPAT)
+    if (!this.constructor.COMPAT) {
       console.error(
         '[ DEPRECATED ] Pear.restart is deprecated and will be removed. Use pear-restart'
       )
-    if (this.#state.ui === null)
+    }
+    if (this.#state.ui === null) {
       throw new Error('Pear.restart is not supported for terminal apps')
+    }
     return ref.track(this.#ipc.restart(opts))
   }
 
   teardown = (fn = () => {}, position = 0) => {
-    if (typeof fn !== 'function')
+    if (typeof fn !== 'function') {
       throw ERR_INVALID_INPUT('teardown expects function')
+    }
 
     const isValidPosition =
       Number.isInteger(position) ||
       position === Infinity ||
       position === -Infinity
-    if (!isValidPosition)
+    if (!isValidPosition) {
       throw ERR_INVALID_INPUT('teardown position must be integer')
+    }
 
     this.#teardowns.push({ fn, position })
   }
@@ -151,8 +158,9 @@ class API {
     this.#unloading()
 
     this.#teardowns.sort((a, b) => a.position - b.position)
-    for (const teardown of this.#teardowns)
+    for (const teardown of this.#teardowns) {
       this.#teardown = this.#teardown.then(teardown.fn)
+    }
 
     const MAX_TEARDOWN_WAIT = 15000
     let timeout = null
@@ -171,12 +179,14 @@ class API {
       rejected = err
     })
     if (timedout || rejected) {
-      if (timedout)
+      if (timedout) {
         console.error(
           `Max teardown wait reached after ${MAX_TEARDOWN_WAIT} ms. Exiting...`
         )
-      if (rejected)
+      }
+      if (rejected) {
         console.error(`${rejected}. User teardown threw. Exiting...`)
+      }
       if (global.Bare) {
         global.Bare.exit()
       } else {
@@ -187,56 +197,65 @@ class API {
   }
 
   message = (msg) => {
-    if (!this.constructor.COMPAT)
+    if (!this.constructor.COMPAT) {
       console.error(
         '[ DEPRECATED ] Pear.message is deprecated and will be removed. Use pear-message'
       )
+    }
     return message(msg)
   }
 
   messages = (pattern, listener) => {
-    if (!this.constructor.COMPAT)
+    if (!this.constructor.COMPAT) {
       console.error(
         '[ DEPRECATED ] Pear.messages is deprecated and will be removed. Use pear-messages'
       )
+    }
     return messages(pattern, listener)
   }
 
   updated = () => {
-    if (!this.constructor.COMPAT)
+    if (!this.constructor.COMPAT) {
       console.error(
         '[ DEPRECATED ] Pear.updated is deprecated and will be removed. It is now a no-op & can be removed from code.'
       )
-    if (typeof this.#ipc.updated === 'function')
+    }
+    if (typeof this.#ipc.updated === 'function') {
       return ref.track(this.#ipc.updated())
+    }
     return Promise.resolve()
   }
 
   reload = async (opts = {}) => {
-    if (!this.constructor.COMPAT)
+    if (!this.constructor.COMPAT) {
       console.error(
         '[ DEPRECATED ] Pear.reload is deprecated and will be removed. Use location.reload.'
       )
-    if (this.#state.ui === null)
+    }
+    if (this.#state.ui === null) {
       throw new Error('Pear.reload is not supported for terminal apps')
-    if (opts.platform)
+    }
+    if (opts.platform) {
       throw new Error('Platform Pear.reload is not supported for desktop apps')
+    }
     global.location.reload()
   }
 
   updates = (listener) => {
-    if (!this.constructor.COMPAT)
+    if (!this.constructor.COMPAT) {
       console.error(
         '[ DEPRECATED ] Pear.updates is deprecated and will be removed. Use pear-updates'
       )
+    }
     return updates(listener)
   }
 
   wakeups = (listener) => {
-    if (!this.constructor.COMPAT)
+    if (!this.constructor.COMPAT) {
       console.error(
         '[ DEPRECATED ] Pear.wakeups is deprecated and will be removed. Use pear-wakeups'
       )
+    }
     return wakeups(listener)
   }
 }
