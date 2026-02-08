fa299a174a6347f045f0d5ae9101fce3621a9da3
Geordan Gesink <georgegeordan@gmail.com>
2026-02-08 21:08:33 +0100

    lint

--- index.mobile.js
+++ index.mobile.js
@@ -21,7 +21,7 @@ const assets = null // TODO: support assets
 Bare.Thread.self.data = data
 
 class API {
-  constructor (opts = {}){
+  constructor(opts = {}) {
     this.isMobile = true
     this.args = opts.args || Bare.argv
     this.argv = null // TODO (maybe the other args (the ones we poped))
@@ -34,58 +34,58 @@ class API {
     this.app.key = null
     this.app.fork = null
     this.app.release = null
-    try{
+    try {
       console.log('parsing link:', link)
       const { hash, drive, query } = plink.parse(link)
       const { key, fork, release, length } = drive
-      const linkInfo = { key, fork, release, length, fragement:hash, query}
+      const linkInfo = { key, fork, release, length, fragement: hash, query }
       this.app = { ...this.app, ...linkInfo } // same as on desktop (its the key of the current thread (on desktop process))
       console.log('logging app:', this.app)
     } catch (err) {
       console.log('error with pear-link:', err)
     }
     this.app = {
-        ...this.app,
-        link, // will be link key for links-map
-        links: {}, // TODO: find out what links is
-        alias: undefined, // TODO: add alias
-        main: opts.main || null,
-        dev: undefined, // TODO (boolean) -> check if its simulator
-        channel: undefined, // TODO (string: stage channel (not just for local, also for remote worker))
-        dht: undefined, // TODO (object {nodes: Array} (DHT nodes))
-        dir: undefined, // TODO (directory of root process (do we need that??))
-        decal: undefined, // TODO (boolean (what is it???))
-        entrypoint: undefined, // TODO (string (entrypoint of the worker))
-        flags: undefined, // maybe in the future (passing flags to the worklet start)
-        linkData: undefined, // TODO (string (what is it??))
-        name: opts.name || null,
-        options: opts.options || null, // TODO (object (pear field as defined in package.json))
-        route: undefined, // TODO (string (what is it??))
-        routes: opts.options?.routes || null,
-        storage: undefined, // User should define (eg expo file system)
-        swapDir: undefined, // seems to point to dir where pear executable is in... do we need it???
-        version: undefined, // TODO (what is it??)
-        stage: undefined, // TODO (what is it??)
-        gui: undefined // do we need that?
+      ...this.app,
+      link, // will be link key for links-map
+      links: {}, // TODO: find out what links is
+      alias: undefined, // TODO: add alias
+      main: opts.main || null,
+      dev: undefined, // TODO (boolean) -> check if its simulator
+      channel: undefined, // TODO (string: stage channel (not just for local, also for remote worker))
+      dht: undefined, // TODO (object {nodes: Array} (DHT nodes))
+      dir: undefined, // TODO (directory of root process (do we need that??))
+      decal: undefined, // TODO (boolean (what is it???))
+      entrypoint: undefined, // TODO (string (entrypoint of the worker))
+      flags: undefined, // maybe in the future (passing flags to the worklet start)
+      linkData: undefined, // TODO (string (what is it??))
+      name: opts.name || null,
+      options: opts.options || null, // TODO (object (pear field as defined in package.json))
+      route: undefined, // TODO (string (what is it??))
+      routes: opts.options?.routes || null,
+      storage: undefined, // User should define (eg expo file system)
+      swapDir: undefined, // seems to point to dir where pear executable is in... do we need it???
+      version: undefined, // TODO (what is it??)
+      stage: undefined, // TODO (what is it??)
+      gui: undefined // do we need that?
     }
 
     this.config = this.app
   }
 
-  checkpoint (state) {
+  checkpoint(state) {
     global.Pear.app.checkpoint = state
     // return ref.track(this.#ipc.checkpoint(state)) // TODO: find equivalent
   }
 
-  versions () {
-    return { runtimes: { bare: Bare.versions.bare }, engines: {}}
+  versions() {
+    return { runtimes: { bare: Bare.versions.bare }, engines: {} }
   }
 
-  exit () {
+  exit() {
     return os.kill(Bare.pid, 'SIGTERM')
   }
 
-  teardown (callback, position){
+  teardown(callback, position) {
     return goodbye(callback, position)
   }
 }
@@ -119,6 +119,6 @@ class API {
 //       prerunning,
 //       version
 
-global.Pear = new API({main, options, name, args})
+global.Pear = new API({ main, options, name, args })
 
 module.exports = API
